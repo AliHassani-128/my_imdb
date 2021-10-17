56f73a0a5301f7b809552d1ad56a17973b4e2a46
AliHassani-128 <alihassani128@yahoo.com>
2021-10-17 11:55:29 +0330

    add urls for register,login,logout

---
+++ user/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from user.api.views import CustomUserCreate,CustomUserLogin,LogoutView
+
+
+urlpatterns = [
+    path('register/', CustomUserCreate.as_view(), name='register'),
+    path('login/', CustomUserLogin.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout_customer'),
+]
