f853e7f69402b04788757fb90f5fa1a15d3a5531
AliHassani-128 <alihassani128@yahoo.com>
2021-10-17 15:09:59 +0330

    config urls for film app api

---
+++ film/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from film.api.views import FilmView, RateFilm
+
+urlpatterns = [
+    path('list-films/', FilmView.as_view(), name='list-films'),
+    path('rate-film/',RateFilm.as_view(),name='rate-film')
+]
