3aa38936fa1d2b544367f6d5c686a5ec65643423
AliHassani-128 <alihassani128@yahoo.com>
2021-10-17 17:28:13 +0330

    reformat film serializer

--- film/api/serializers.py
+++ film/api/serializers.py
@@ -4,15 +4,13 @@ from film.models import Film, Film_Rate_User
 
 
 class FilmSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Film
         depth = 1
-        fields = ['name','director','pub_date','description','mean_rate','file']
+        fields = ['name', 'director', 'pub_date', 'description', 'mean_rate', 'file']
 
 
 class FilmRateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Film_Rate_User
         fields = '__all__'
-
