26f6c8f6ee4ee5f33ae522e393cead760522eabe
AliHassani-128 <alihassani128@yahoo.com>
2021-10-17 13:54:35 +0330

    fix urls for custom user

--- user/api/urls.py
+++ user/api/urls.py
@@ -5,5 +5,5 @@ from user.api.views import CustomUserCreate,CustomUserLogin,LogoutView
 urlpatterns = [
     path('register/', CustomUserCreate.as_view(), name='register'),
     path('login/', CustomUserLogin.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout_customer'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
