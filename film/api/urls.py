58beb728c96559ef173d4bc9de35693d426c8fc1
AliHassani-128 <alihassani128@yahoo.com>
2021-10-17 16:47:06 +0330

    add url for search view

--- film/api/urls.py
+++ film/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from film.api.views import FilmView, RateFilm
+from film.api.views import FilmView, RateFilm, SearchFilm
 
 urlpatterns = [
     path('list-films/', FilmView.as_view(), name='list-films'),
-    path('rate-film/',RateFilm.as_view(),name='rate-film')
+    path('rate-film/',RateFilm.as_view(),name='rate-film'),
+    path('search/',SearchFilm.as_view(),name='search')
 ]
