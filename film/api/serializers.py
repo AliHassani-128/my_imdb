d740ec1ab6e398943506b834086a1ed4c877cc17
AliHassani-128 <alihassani128@yahoo.com>
2021-10-17 15:09:38 +0330

    set serializers for films models

---
+++ film/api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from film.models import Film, Film_Rate_User
+
+
+class FilmSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Film
+        depth = 1
+        fields = ['name','director','pub_date','description','mean_rate','file']
+
+
+class FilmRateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Film_Rate_User
+        fields = '__all__'
+
